a375a89a3350b2aa7c25d68ab5f889043aad086e
luizandrenf <luizandrenf@gmail.com>
2024-07-01 09:34:01 -0400

    feat: delete-answer use case

---
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { DeleteAnswerUseCase } from './delete-answer'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: DeleteAnswerUseCase
+describe('Deelete Answer', async () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new DeleteAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to delete a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityId('any-author'),
+      },
+      new UniqueEntityId('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      answerId: 'answer-1',
+      authorId: 'any-author',
+    })
+
+    expect(inMemoryAnswersRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityId('any-author'),
+      },
+      new UniqueEntityId('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await expect(async () => {
+      await sut.execute({
+        answerId: 'answer-1',
+        authorId: 'any-author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
