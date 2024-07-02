9b528e2de0eadd2c5c66f78275d387ec1b88685c
luizandrenf <luizandrenf@gmail.com>
2024-07-02 11:20:38 -0400

    feat: fetch-question-comments use case created

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,29 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+interface FetchQuestionCommentsUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionCommentsUseCaseResposnse {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResposnse> {
+    const questionComments =
+      await this.questionCommentsRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+
+    return {
+      questionComments,
+    }
+  }
+}
