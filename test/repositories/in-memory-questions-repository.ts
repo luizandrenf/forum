1d46674621ecfe9903e3f9083d96d7236c84e1da
luizandrenf <luizandrenf@gmail.com>
2024-07-04 09:28:49 -0400

    feat: send a notification when best answer is chosen

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -30,12 +31,6 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question
   }
 
-  async save(question: Question) {
-    const itemIndex = this.items.findIndex((item) => item.id === question.id)
-
-    this.items[itemIndex] = question
-  }
-
   async findManyRecent({ page }: PaginationParams) {
     const questions = this.items
       .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
@@ -44,8 +39,18 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return questions
   }
 
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items[itemIndex] = question
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
+  }
+
   async create(question: Question) {
     this.items.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete(question: Question) {
